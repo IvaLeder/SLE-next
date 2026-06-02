763c48aaff297282f439cd4f4e268a14a96a140c
Iva <star_forsaken_000@yahoo.co.uk>
2026-06-02 08:33:47 +0200

    Add favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,13 +12,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#4f46e5",
     icons: [
       {
-        src: "/icon",
-        sizes: "32x32",
+        src: "/images/icon-192.png",
+        sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/apple-icon",
-        sizes: "180x180",
+        src: "/images/icon-512.png",
+        sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
